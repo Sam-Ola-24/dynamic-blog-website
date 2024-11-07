9b02a5f39beee79feffd8b72fdabb460930c6060
Sam-Ola-24 <toye.akinola@gmail.com>
2024-11-07 16:21:12 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,72 +1,79 @@
-
 const posts = [];
-let save = document.getElementById("save-btn");
+const submitError =
+  "Please confirm that the fields are not empty and try again!";
+const save = document.getElementById("save-btn");
+const edit = document.getElementById("edit-btn");
+const remove = document.getElementById("delete-btn");
 
-if(save){
-save.addEventListener("click", savePost);} else{
+if (save) {
+  save.addEventListener("click", savePost);
+} else {
   //do nothing
-};
+}
 
 //To execute the save action
 function savePost() {
-    if((!document.getElementById("title").value) || (!document.getElementById("content").value)){
-        document.getElementById("error").innerHTML = "Please confirm that the fields are not empty and try again!";
-    } else{
-  let postTitle = document.getElementById("title").value;
-  localStorage.setItem("title", postTitle);
-console.log(postTitle);
+  if (
+    !document.getElementById("title").value ||
+    !document.getElementById("content").value
+  ) {
+    document.getElementById("error").innerHTML = submitError;
+  } else {
+    let postTitle = document.getElementById("title").value;
+    localStorage.setItem("title", postTitle);
 
-  let postContent = document.getElementById("content").value;
-  localStorage.setItem("content", postContent);
-  console.log(postContent);
-
-  posts.push({postTitle, postContent});
-
-  document.getElementById("displayTitle").innerHTML = postTitle;
-  document.getElementById("displayContent").innerHTML = postContent;
-
-};
-};
-
-
-for(i=0; i < posts.length; i++){
-  document.getElementById("postContent").append(
-    "<tr><td>"+i.title+"</td><td>"+i.content+"</td></tr>"
-  );
+    let postContent = document.getElementById("Content").value;
+    localStorage.setItem("content", postContent);
+  }
 }
 
+let titleDisplay = document.getElementById("displayTitle");
+let contentDisplay = document.getElementById("displayContent");
 
-for(i=0; i < posts.length; i++){
-  document.getElementById("list-post").append(
-    "<tr><td>"+i.title+"</td><td>"+i.content+" <button id='edit-btn'>Edit</button>"+" <button id='delete-btn'>Delete</button></tr>"
-  );
+let postTitle = localStorage.getItem("title");
+if (titleDisplay) {
+  titleDisplay.innerHTML = "<strong>Title: </strong>" + postTitle;
 }
 
-// let postList = document.getElementById("list-post");
-// postList.innerHTML.append(postData+"  <button id='edit-btn'>Edit</button>"+" <button id='delete-btn'>Delete</button>");
-
-const edit = document.getElementById("edit-btn");
-
-if(edit){
-edit.addEventListener("click", editPost);} else{
-//do nothing
+let postContent = localStorage.getItem("content");
+if (contentDisplay) {
+  contentDisplay.innerHTML = "<strong>Content:</strong> " + postContent;
 }
 
-function editPost(event){
+posts.push({ postTitle, postContent });
 
-};
+//done saving to array
+const list = document.getElementById("postContent");
+const postList = document.getElementById("list-post");
 
-
-const remove = document.getElementById("delete-btn");
-
-if(remove){
-remove.addEventListener("click", deletePost); }else{
-  //do nothing
+for (i = 0; i < posts.length; i++) {
+  console.log(posts[i].postTitle);
+  if (list) {
+    list.innerHTML =
+      "<tr><td>" +
+      posts[i].postTitle +
+      "</td><td>" +
+      posts[i].postContent +
+      "</td></tr>";
+  }
 }
 
-function deletePost(event){
-  postList.innerHTML.remove();
-  showAlert("Post has been deleted", "danger");
+for (i = 0; i < posts.length; i++) {
+  console.log(posts[i].postTitle);
+  if (postList) {
+    postList.innerHTML =
+      "<tr><td>" +
+      posts[i].postTitle +
+      "</td><td>" +
+      posts[i].postContent +
+      "<button id='edit-btn'>Edit Post</button><button id='delete-btn'>Delete Post</button></td></tr>";
+  }
+}
 
-};
+//Edit button logic
 
+if (edit) {
+  edit.addEventListener("click", editPost);
+}
+
+function editPost() {}
